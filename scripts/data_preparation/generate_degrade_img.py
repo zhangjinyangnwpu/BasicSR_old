59c9acba70383a5578912d646d871e8dfdcc9150
zhangjinyangnwpu <ttblue888@gmail.com>
2022-05-06 20:07:29 +0800

    add aniso_gaussian degerate

--- scripts/data_preparation/generate_degrade_img.py
+++ scripts/data_preparation/generate_degrade_img.py
@@ -1,17 +1,31 @@
 import os
 import glob
+
+import cv2
 from PIL import Image
 import torchvision
+import scipy.io as sio
+from matplotlib import pyplot as plt
+import scipy.misc
+import imageio
 
 from basicsr.utils import SRMDPreprocessing
 
 # 在代码修改开头的数据配置，然后代码根目录，执行 python scripts/data_preparation/generate_degrade_img.py
 
 root_path = './datasets' # 修改这个目录，指定为数据的根目录，可以用 ln -s 指令和代码目录的datasets文件夹关联
-dataset_names = ['Urban100'] # div2k set5 set14 B100 Urban100 可以处理的数据集
-scales = [2,3,4] # 不同大小的scale
+dataset_names = ['set5'] # div2k set5 set14 B100 Urban100 可以处理的数据集
+scales = [2] # 不同大小的scale
+blur_types = ['aniso_gaussian']  # iso_gaussian or aniso_gaussian，高斯核类型，aniso_gaussian还未处理
+
+# for iso_gaussian
 kernel_widths = [0,0.6,1.2,1.8] # 不同的同向高斯核宽度
-blur_types = ['iso_gaussian']  # iso_gaussian or aniso_gaussian，高斯核类型，aniso_gaussian还未处理
+
+# for aniso_gaussian
+lambda1s = [0.5]
+lambda2s = [3]
+thetas = [2]
+noises = [30]
 
 for dataset_name in dataset_names:
     if dataset_name == 'div2k':
@@ -45,14 +59,6 @@ for dataset_name in dataset_names:
 
     os.makedirs(save_path_root, exist_ok=True)
     mode = 'bicubic'
-    sig_min = 0.2  # training 0.2 for x2, 0.2 for x3, 0.2 for x4 for iso_gaussian
-    sig_max = 2.0  # training 2.0 for x2, 3.0 for x2, 4.0 for x4 for iso_gaussian
-    lambda_1 = 0.2  # test with a cetrain value for aniso_gaussian
-    lambda_2 = 4.0  # test with a cetrain value for aniso_gaussian
-    theta = 0  # angle for aniso_gaussian, set with angle when testing
-    lambda_min = 0.2  # training 0.2 for x2,x3,x4 for aniso_gaussian
-    lambda_max = 4.0  # training 4.0 for x2,x3,x4 for aniso_gaussian
-    noise = 0  # random for training and testing for valiation
 
     for scale in scales:
         for blur_type in blur_types:
@@ -69,29 +75,71 @@ for dataset_name in dataset_names:
                         kernel_size=kernel_size,
                         blur_type=blur_type,
                         sig=sig,
-                        sig_min=sig_min,
-                        sig_max=sig_max,
-                        lambda_1=lambda_1,
-                        lambda_2=lambda_2,
-                        theta=theta,
-                        lambda_min=lambda_min,
-                        lambda_max=lambda_max,
-                        noise=noise
+                        sig_min=0.2,
+                        sig_max=2.0,
+                        lambda_1=0.2,
+                        lambda_2=0.4,
+                        theta=0,
+                        lambda_min=0.2,
+                        lambda_max=4.0,
+                        noise=0
                     )
 
                     for name in hr_names:
                         hr = Image.open(name)
                         hr = torchvision.transforms.ToTensor()(hr)
                         hr = hr.unsqueeze(0).cuda()
-                        lr, _ = degrade(hr, random=False)
+                        lr, kernel = degrade(hr, random=False)
+                        kernel = kernel.cpu()[0]
                         print(hr.shape,lr.shape)
                         lr = lr.squeeze(0).cpu()
                         lr_img = torchvision.transforms.ToPILImage()(lr)
                         lr_img.save(os.path.join(save_path, os.path.basename(name)))
+                        plt.clf()
+                        plt.figure(figsize=(5, 5))
+                        plt.imshow(kernel, interpolation='bicubic')
+                        plt.savefig(os.path.join(save_path, 'kernel.jpg'), cmap='Greys')
                         # break
             elif blur_type == 'aniso_gaussian':
-                kernel_size = 11  # gaussian kernel size
-                degrade_type = '{}_lambda1_{}_lambda2_{}_theta_{}'.format(blur_type,lambda_1,lambda_2,theta)
+                kernel_size = 25  # gaussian kernel size
+                for lambda1 in lambda1s:
+                    for lambda2 in lambda2s:
+                        for theta in thetas:
+                            for noise in noises:
+                                degrade_type = '{}_lambda1_{}_lambda2_{}_theta_{}_noise_{}'.format(blur_type, lambda1,lambda2,theta,noise)
+                                save_path = os.path.join(save_path_root, 'X{}'.format(scale), degrade_type)
+                                os.makedirs(save_path, exist_ok=True)
+                                degrade = SRMDPreprocessing(
+                                    scale=scale,
+                                    mode=mode,
+                                    kernel_size=kernel_size,
+                                    blur_type=blur_type,
+                                    sig=0,
+                                    sig_min=0.2,
+                                    sig_max=2.0,
+                                    lambda_1=lambda1,
+                                    lambda_2=lambda2,
+                                    theta=theta,
+                                    lambda_min=0.2,
+                                    lambda_max=4.0,
+                                    noise=noise
+                                )
+                                for name in hr_names:
+                                    hr = Image.open(name)
+                                    hr = torchvision.transforms.ToTensor()(hr)
+                                    hr = hr.unsqueeze(0).cuda()
+                                    lr, kernel = degrade(hr, random=False)
+                                    kernel = kernel.cpu()[0]
+                                    print(hr.shape, lr.shape,kernel.shape)
+                                    lr = lr.squeeze(0).cpu()
+                                    lr_img = torchvision.transforms.ToPILImage()(lr)
+                                    lr_img.save(os.path.join(save_path, os.path.basename(name)))
+                                    sio.savemat(os.path.join(save_path,'kernel.mat'),{'kernel':kernel})
+                                    plt.clf()
+                                    plt.figure(figsize=(5, 5))
+                                    plt.imshow(kernel,interpolation='bicubic')
+                                    plt.savefig(os.path.join(save_path,'kernel.jpg'),cmap='Greys')
+
             else:
                 raise TypeError
 
